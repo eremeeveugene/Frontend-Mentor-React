39064086970a20e2512c58f3aad72570d4286a42
Eugene <eremeeveugene@gmail.com>
2025-07-28 19:11:07 +0300

    setup

---
+++ source/qr-code-component/src/components/QrCodeComponent/QrCodeComponent.jsx
@@ -0,0 +1,11 @@
+import "./QrCodeComponent.css";
+
+function QrCodeComponent() {
+  return (
+    <div className="container">
+      <div className="inner"></div>
+    </div>
+  );
+}
+
+export default QrCodeComponent;

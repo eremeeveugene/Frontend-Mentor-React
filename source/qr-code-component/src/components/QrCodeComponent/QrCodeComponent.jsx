8625e6eebdbf09fdc11be56217f057558cc291d4
Eugene <eremeeveugene@gmail.com>
2025-07-28 21:33:58 +0300

    implementation

--- source/qr-code-component/src/components/QrCodeComponent/QrCodeComponent.jsx
+++ source/qr-code-component/src/components/QrCodeComponent/QrCodeComponent.jsx
@@ -3,11 +3,21 @@ import "./QrCodeComponent.css";
 function QrCodeComponent() {
   return (
     <div className="card">
-      <div className="image-card"></div>
+      <div className="image-card">
+        <div className="ellipse ellipse--top-left"></div>
+        <div className="ellipse ellipse--bottom-right"></div>
+
+        <img
+          className="qr-code-image"
+          src="/images/qr-code.svg"
+          alt="QR Code"
+        />
+      </div>
 
       <h4 className="text-preset-1 title">
         Improve your front-end skills by building projects
       </h4>
+
       <p className="text-preset-2 caption">
         Scan the QR code to visit Frontend Mentor and take your coding skills to
         the next level

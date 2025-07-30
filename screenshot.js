0d34e4dfed8a6dbfc12cb53e5de98e12a2dfe493
Eugene <eremeeveugene@gmail.com>
2025-07-30 22:09:08 +0300

    add icon

--- screenshot.js
+++ screenshot.js
@@ -4,9 +4,10 @@ const puppeteer = require("puppeteer");
   const browser = await puppeteer.launch();
   const page = await browser.newPage();
 
-  await page.setViewport({ width: 375, height: 812 });
+  //await page.setViewport({ width: 375, height: 812 });
+  await page.setViewport({ width: 1440, height: 960 });
   await page.goto("http://localhost:5173");
 
-  await page.screenshot({ path: "export.png" });
+  await page.screenshot({ path: "actual result.png" });
   await browser.close();
 })();
